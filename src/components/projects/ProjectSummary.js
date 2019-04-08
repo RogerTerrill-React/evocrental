28157d4c65887ffa4a2d606f1436e998b8f67c67
Roger Terrill <rchicasterill@gmail.com>
2019-04-08 14:23:02 -0700

    12-Adding Dummy Data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = props => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{props.title}</span>
+        <span className="card-title">{project.title}</span>
         <p>Post by the Net Ninja</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
