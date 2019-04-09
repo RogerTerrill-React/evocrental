3164ffdbe5ddd126ccb6041de74bcec8e15d9ca9
Roger Terrill <rchicasterill@gmail.com>
2019-04-09 09:59:30 -0700

    18-Adding Data to Firestore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -12,6 +12,6 @@ var config = {
   messagingSenderId: '206838482278'
 };
 firebase.initializeApp(config);
-firebase.firestore().settings({timestampsInSnapshots: true})
+firebase.firestore().settings({})
 
 export default firebase;
