58c008b586beb7657475ea79025e3eb87318bb34
Roger Terrill <rchicasterill@gmail.com>
2019-04-09 13:15:46 -0700

    22-Firebase Auth with Redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
 // Syncs firestore date with redux state
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
