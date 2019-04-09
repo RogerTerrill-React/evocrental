b4a1b2228a19246d25be8afba58ee9e55760ff2e
Roger Terrill <rchicasterill@gmail.com>
2019-04-09 15:42:48 -0700

    31-Formatting Dates with Moment

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { createProject } from '../../store/actions/projectActions'
-import {Redirect} from 'react-router-dom';
+import { createProject } from '../../store/actions/projectActions';
+import { Redirect } from 'react-router-dom';
 
 class CreateProject extends Component {
   state = {
@@ -17,12 +17,13 @@ class CreateProject extends Component {
   handleSubmit = e => {
     e.preventDefault();
     //console.log(this.state);
-    this.props.createProject(this.state)
+    this.props.createProject(this.state);
+    this.props.history.push('/');
   };
 
   render() {
     const { auth } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' />
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -33,7 +34,11 @@ class CreateProject extends Component {
           </div>
           <div className="input-field">
             <label htmlFor="content">Project Content</label>
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Create</button>
@@ -47,13 +52,16 @@ class CreateProject extends Component {
 const mapStateToProps = state => {
   return {
     auth: state.firebase.auth
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
+    createProject: project => dispatch(createProject(project))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateProject);
