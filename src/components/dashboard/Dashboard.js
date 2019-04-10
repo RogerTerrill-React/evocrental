3308f490e7c147a84421a000fca8ccfd522f5535
Roger Terrill <rchicasterill@gmail.com>
2019-04-10 09:39:07 -0700

    38-Showing Notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,12 +4,12 @@ import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' />
+    const { projects, auth, notifications } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
       <div className="dashboard container">
@@ -18,7 +18,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications}/>
           </div>
         </div>
       </div>
@@ -30,11 +30,15 @@ const mapStateToProps = state => {
   console.log(state);
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   };
 };
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: 'projects' }]) // Syncs the state of redux with particular collection
+  firestoreConnect([
+    { collection: 'projects' },
+    { collection: 'notifications', limit: 3 }
+  ]) // Syncs the state of redux with particular collection
 )(Dashboard);
