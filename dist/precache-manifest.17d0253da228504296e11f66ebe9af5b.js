63dad873b272e0414ea4420a8099977edd8401bd
Roger Terrill <rchicasterill@gmail.com>
2019-04-10 09:50:38 -0700

    40-Deploy to firebase

---
+++ dist/precache-manifest.17d0253da228504296e11f66ebe9af5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9bf93aa5ddb4712c3eac",
+    "url": "/static/js/main.9a36111e.chunk.js"
+  },
+  {
+    "revision": "4f59284a18f7dddb59e3",
+    "url": "/static/js/2.a1bed74c.chunk.js"
+  },
+  {
+    "revision": "9bf93aa5ddb4712c3eac",
+    "url": "/static/css/main.fbd3d544.chunk.css"
+  },
+  {
+    "revision": "1089ec38d2be6b730a401db535cd3c64",
+    "url": "/index.html"
+  }
+];
